b820b003ca1854207bcbd95629398de6396ecaf9
Alvie Stoddard <alviestoddard@gmail.com>
2020-09-06 18:25:54 -0400

    .

--- code-site/src/App.js
+++ code-site/src/App.js
@@ -2,30 +2,38 @@ import React from "react";
 import ReactDom from "react-router-dom";
 import logo from "./logo.svg";
 import './App.css';
-import  AppBar from '@material-ui/core/AppBar';
-import  Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import { AppBar, Button, Toolbar, IconButton, Typography } from '@material-ui/core'; 
+import { makeStyles } from '@material-ui/core/styles';
 
-import  IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-// import  MenuIcon from '@material-ui/icons/MenuIcon';
-import  Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <AppBar position="static">
          <Toolbar>
          <IconButton
             edge="start"
-            // className={classes.menuButton}
+            className={classes.menuButton}
             color="inherit"
             aria-label="menu"
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" >
+          <Typography variant="h6" className={classes.title}>
             News
           </Typography>
           <Button color="inherit">Login</Button>
