6b90348db36529ac44a64f2c493c2efe23b1e6b9
Gibson Munene <njine10@gmail.com>
2021-01-27 21:08:18 -0500

    Fixed runtime errors

--- code-site/src/Routes.js
+++ code-site/src/Routes.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./Components/Landing/Home";
 import AboutUs from "./Components/About Us/AboutUs";
-import ContactUs from "./Components/Contact Us/ContactUsPage";
-import Partners from "./Components/Partners/PartnersPage";
+// import ContactUs from "./Components/ContactUs/ContactUsPage";
+// import Partners from "./Components/Partners/PartnersPage";
 import Residents from "./Components/Residents/residentspage";
-import Resources from "./Components/Resources/Resources";
+// import Resources from "./Components/Resources/Resources";
 // import "../styles/Style.css"
 import NoMatch from "./Components/Other/NoMatch";
 import ComingSoon from "./Components/Other/ComingSoon"
