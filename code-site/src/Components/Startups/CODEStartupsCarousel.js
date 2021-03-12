2301fee1fac8d16535bd51e4cde1ff5fc594b01a
Alvie Stoddard <alviestoddard@gmail.com>
2021-03-12 16:06:46 -0500

    /

--- code-site/src/Components/Startups/CODEStartupsCarousel.js
+++ code-site/src/Components/Startups/CODEStartupsCarousel.js
@@ -30,72 +30,83 @@ function CarouselDemo() {
     },
   ];
 
-//   const productService = new ProductService();
-//   console.log("up here");
+  //   const productService = new ProductService();
+  //   console.log("up here");
 
   useEffect(() => {
     let data = {
       data: [
         {
           id: "1",
-          name: "Alvie",
-          company: "Bamboo Watch",
-          description: "Product Description",
-          image: "bamboo-watch.jpg",
-          price: 65,
-          category: "Accessories",
-          quantity: 24,
-          inventoryStatus: "INSTOCK",
-          rating: 5,
+          company_name: "XHighlight",
+          founders: "Alvie Stoddard",
+          description:
+            "The Fastest and Easiest Way to make your Sports Highlight Videos",
+          image: "XHighlght.png",
+          //   price: 65,
+          website: "xhighlight.com",
+          custom_field_one: "",
+          custom_field_two: "",
+          other_info: "",
         },
         {
           id: "2",
-          name: "Jay",
-          company: "Black Watch",
-          description: "Product Description",
-          image: "black-watch.jpg",
-          price: 72,
-          category: "Accessories",
-          quantity: 61,
-          inventoryStatus: "INSTOCK",
-          rating: 4,
+          company_name: "Onleye",
+          founders: "Julian Wiley",
+          description:
+            "Reinventing Car Security",
+          image: "onleye.png",
+          //   price: 65,
+          website: "onleye.com",
+          //   quantity: 24,
+          //   inventoryStatus: "INSTOCK",
+          custom_field_one: "",
+          custom_field_two: "",
+          other_info: "",
         },
         {
-            id: "3",
-            name: "Jay",
-            company: "Black Watch",
-            description: "Product Description",
-            image: "black-watch.jpg",
-            price: 72,
-            category: "Accessories",
-            quantity: 61,
-            inventoryStatus: "INSTOCK",
-            rating: 4,
-          },
-          {
-            id: "4",
-            name: "Jay",
-            company: "Black Watch",
-            description: "Product Description",
-            image: "black-watch.jpg",
-            price: 72,
-            category: "Accessories",
-            quantity: 61,
-            inventoryStatus: "INSTOCK",
-            rating: 4,
-          },
-          {
-            id: "5",
-            name: "Jay",
-            company: "Black Watch",
-            description: "Product Description",
-            image: "black-watch.jpg",
-            price: 72,
-            category: "Accessories",
-            quantity: 61,
-            inventoryStatus: "INSTOCK",
-            rating: 4,
-          },
+          id: "3",
+          company_name: "Combust-EV",
+          founders: "Tommy Croissant",
+          description:
+            "Converting Gas to Electric Vehicles",
+          image: "XHighlght.png",
+          //   price: 65,
+          website: "combust-ev.com",
+          //   quantity: 24,
+          //   inventoryStatus: "INSTOCK",
+          custom_field_one: "",
+          custom_field_two: "",
+          other_info: "",
+        },
+        {
+          id: "4",
+          company_name: "VinciVR",
+          founders: "Eagle Wu",
+          description:
+            "Reinveinting Trainnig Through Virtual Reality",
+          image: "VinciVR.png",
+          //   price: 65,
+          website: "vincivr.com",
+          //   quantity: 24,
+          //   inventoryStatus: "INSTOCK",
+          custom_field_one: "",
+          custom_field_two: "",
+          other_info: "",
+        },
+        {
+          id: "5",
+          company_name: "XHighlight",
+          founders: "Alvie Stoddard",
+          description:
+            "The Fastest and Easiest Way to make your Sports Highlight Videos",
+          image: "XHighlght.png",
+          //   price: 65,
+          website: "xhighlight.com",
+          custom_field_one: "",
+          custom_field_two: "",
+          other_info: "",
+        },
       ],
     };
     setProducts(data.data.slice(0, 9));
@@ -106,44 +117,46 @@ function CarouselDemo() {
     console.log("product");
     return (
       <div className="product-item">
-        <div className="product-item-content">
-          <div className="p-mb-3">
-            <img
-              src={
-                "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
-              }
-              onError={(e) =>
-                (e.target.src =
-                  "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
-              }
-              alt={"product.name"}
-              className="product-image"
-            />
-          </div>
-          <div>
-            <h4 className="p-mb-1">{product.name}</h4>
-            <h6 className="p-mt-0 p-mb-3">${"product.price"}</h6>
-            <span
-              className={`product-badge status-${"product.inventoryStatus.toLowerCase()"}`}
-            >
-              {"product.inventoryStatus"}
-            </span>
-            <div className="car-buttons p-mt-5">
-              <Button
-                icon="pi pi-search"
-                className="p-button p-button-rounded p-mr-2"
-              />
-              <Button
-                icon="pi pi-star"
-                className="p-button-success p-button-rounded p-mr-2"
-              />
-              <Button
-                icon="pi pi-cog"
-                className="p-button-help p-button-rounded"
+        <a href="/">
+          <div className="product-item-content">
+            <div className="p-mb-3">
+              <img
+                src={
+                  "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
+                }
+                onError={(e) =>
+                  (e.target.src =
+                    "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+                }
+                alt={"product.name"}
+                className="product-image"
               />
             </div>
+            <div>
+              <h4 className="p-mb-1">{product.name}</h4>
+              <h6 className="p-mt-0 p-mb-3">${"product.price"}</h6>
+              <span
+                className={`product-badge status-${"product.inventoryStatus.toLowerCase()"}`}
+              >
+                {"product.inventoryStatus"}
+              </span>
+              <div className="car-buttons p-mt-5">
+                <Button
+                  icon="pi pi-search"
+                  className="p-button p-button-rounded p-mr-2"
+                />
+                <Button
+                  icon="pi pi-star"
+                  className="p-button-success p-button-rounded p-mr-2"
+                />
+                <Button
+                  icon="pi pi-cog"
+                  className="p-button-help p-button-rounded"
+                />
+              </div>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     );
   };
@@ -175,7 +188,6 @@ function CarouselDemo() {
           header={<h5>Circular, AutoPlay, 3 Items per Page and Scroll by 1</h5>}
         />
       </div>
-
       {/* <div className="card">
         <Carousel
           value={products}
